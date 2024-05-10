5c724ee171a63938afe5e97c99a18d2edd7be379
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2024-05-10 14:55:20 -0400

    register payment endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PaymentController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Authentication
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+// Payments
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/payments/initiate', [PaymentController::class, 'initiate']);
+    Route::get('/payments/enquiry/{paymentId}', [PaymentController::class, 'enquiry']);
+});
