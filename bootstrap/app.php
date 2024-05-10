884ebe637550909a38448068cb3016131ed679a7
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2024-05-10 14:34:59 -0400

    register middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
+        $middleware->append(\App\Http\Middleware\EnsureAcceptJsonRequest::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
