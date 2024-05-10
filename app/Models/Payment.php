1e6662d42cadf639c360590c0e4ed3e122877920
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2024-05-10 19:20:19 -0400

    setup UUIDs

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,13 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Payment extends Model
 {
     protected $fillable = ['user_id', 'status', 'amount', 'transaction_id'];
 
+    protected $keyType = 'string';
+    public $incrementing = false;
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function booted() {
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
     }
 }
