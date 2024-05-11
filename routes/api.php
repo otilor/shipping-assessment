58dd23a3e29db97f3ceffc3a2f1b58c0258a0224
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2024-05-11 16:30:26 -0400

    register api endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PaymentController;
-
+use App\Http\Controllers\DeliveryController;
+use App\Http\Controllers\ShippingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,4 +20,20 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/payments/initiate', [PaymentController::class, 'initiate']);
     Route::get('/payments/enquiry/{paymentId}', [PaymentController::class, 'enquiry']);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Shipments CRUD
+    Route::get('/shipments', [ShippingController::class, 'index']);
+    Route::post('/shipments', [ShippingController::class, 'store']);
+    Route::get('/shipments/{id}', [ShippingController::class, 'show']);
+    Route::put('/shipments/{id}', [ShippingController::class, 'update']);
+    Route::delete('/shipments/{id}', [ShippingController::class, 'destroy']);
+
+    // Deliveries CRUD
+    Route::get('/deliveries', [DeliveryController::class, 'index']);
+    Route::post('/deliveries', [DeliveryController::class, 'store']);
+    Route::get('/deliveries/{id}', [DeliveryController::class, 'show']);
+    Route::put('/deliveries/{id}', [DeliveryController::class, 'update']);
+    Route::delete('/deliveries/{id}', [DeliveryController::class, 'destroy']);
 });
